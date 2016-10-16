52bcfb53d6688f88c76ab9baa6c899c9bd26abd5
Heikki Pora <heikki.pora@reaktor.fi>
2016-10-16 11:18:33 +0300

    Pretty-print metadata in tests

--- test/sync.sh
+++ test/sync.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-time ../bin/sync --prune --manifest ./package.json --root ./download --localUrl https://localhost:8443 --binaryAbi 46,47,48 --binaryArch x64 --binaryPlatform linux,darwin
-
+time ../bin/sync --prune --pretty --manifest ./package.json --root ./download --localUrl https://localhost:8443 --binaryAbi 46,47,48 --binaryArch x64 --binaryPlatform linux,darwin
