6497ebbee18a2933b373a6384f9fb75c37178fb9
Heikki Pora <heikki.pora@gmail.com>
2018-07-18 15:49:31 +0300

    Update manual test script sync.sh to use newer ABI versions

--- test/manual/sync.sh
+++ test/manual/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../node_modules/.bin/babel-node ../../bin/sync --manifest ./package-lock.json --root ./.download --localUrl https://localhost:8443 --binaryAbi 48,57 --binaryArch x64 --binaryPlatform darwin
+../../node_modules/.bin/babel-node ../../bin/sync --manifest ./package-lock.json --root ./.download --localUrl https://localhost:8443 --binaryAbi 57,64 --binaryArch x64 --binaryPlatform darwin
