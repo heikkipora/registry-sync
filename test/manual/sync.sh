86ef8ed0cc0221422501c01dbde5446e413efb86
Heikki Pora <heikki.pora@gmail.com>
2018-10-07 21:59:28 +0300

    Fix manual tests
    Use production build of registry-sync

--- test/manual/sync.sh
+++ test/manual/sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-../../node_modules/.bin/babel-node ../../bin/sync --manifest ./package-lock.json --root ./.download --localUrl https://localhost:8443 --binaryAbi 57,64 --binaryArch x64 --binaryPlatform darwin
+(cd ../../ && npm run build)
+../../build/bin/sync --manifest ./package-lock.json --root ./.download --localUrl https://localhost:8443 --binaryAbi 57,64 --binaryArch x64 --binaryPlatform darwin
