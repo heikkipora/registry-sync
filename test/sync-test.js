a2e753a87de1129445488b7d5305863ce39bc2ee
Heikki Pora <heikki.pora@gmail.com>
2019-11-23 14:42:06 +0200

    Update packages used in sync testing to latest versions. Update Node ABI versions

--- test/sync-test.js
+++ test/sync-test.js
@@ -7,8 +7,8 @@ const rootFolder = `${__dirname}/.download`
 const prebuiltBinaryProperties = [
   {abi: 57, arch: 'x64', platform: 'darwin'},
   {abi: 57, arch: 'x64', platform: 'linux'},
-  {abi: 48, arch: 'x64', platform: 'darwin'},
-  {abi: 48, arch: 'x64', platform: 'linux'}
+  {abi: 64, arch: 'x64', platform: 'darwin'},
+  {abi: 64, arch: 'x64', platform: 'linux'}
 ]
 
 const options = {
@@ -22,7 +22,7 @@ const options = {
 describe('synchronize', () => {
   it('Should download a bunch of packages', async () => {
     const downloaded = await synchronize(options)
-    expect(downloaded).to.have.lengthOf(121)
+    expect(downloaded).to.have.lengthOf(123)
   })
 
   it('Should already have all of the packages', async () => {
@@ -31,12 +31,9 @@ describe('synchronize', () => {
   })
 
   it('Should detect a change to pre-built binary properties and re-trigger download', async () => {
-    const downloaded = await synchronize({...options, prebuiltBinaryProperties: prebuiltBinaryProperties.concat({abi: 64, arch: 'x64', platform: 'darwin'})})
-    expect(downloaded).to.have.lengthOf(121)
+    const downloaded = await synchronize({...options, prebuiltBinaryProperties: prebuiltBinaryProperties.concat({abi: 72, arch: 'x64', platform: 'darwin'})})
+    expect(downloaded).to.have.lengthOf(123)
   })
 
   after(done => rimraf(rootFolder, done))
 })
-
-
-
