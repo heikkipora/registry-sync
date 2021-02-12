a9507eb6b320f6a92d47e476068b2f0c7312f0e5
Juha Helminen <juhahe@iki.fi>
2021-02-12 11:12:01 +0200

    Add prettier for formatting code

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
     ],
     "rules": {
         "@typescript-eslint/no-empty-function": "off",
