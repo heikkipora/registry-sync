c74c52fc667e8834b51d62617b836fa4966cb3ad
Heikki Pora <heikki.pora@gmail.com>
2026-01-05 13:32:23 +0200

    Migrate release-test from js to ts

--- release-test/server/src/server.ts
+++ release-test/server/src/server.ts
@@ -1,30 +1,31 @@
-const express = require('express')
-const fs = require('fs')
-const http = require('http')
-const https = require('https')
-const path = require('path')
+import express from 'express'
+import fs from 'fs'
+import http from 'http'
+import https from 'https'
+import path from 'path'
+import type {Express, Response} from 'express'
 
-function startHttpServer(app, port) {
-  const httpServer = http.Server(app)
+function startHttpServer(app: Express, port: number): void {
+  const httpServer = new http.Server(app)
   httpServer.listen(port, () => console.log(`Listening on HTTP port *:${port}`))
 }
 
-function startHttpsServer(app, port, sslKey, sslCert) {
+function startHttpsServer(app: Express, port: number, sslKey: string, sslCert: string): void {
   const privateKey = fs.readFileSync(sslKey, 'utf8')
   const certificate = fs.readFileSync(sslCert, 'utf8')
-  const httpsServer = https.Server({key: privateKey, cert: certificate}, app)
+  const httpsServer = new https.Server({key: privateKey, cert: certificate}, app)
   httpsServer.listen(port, () => console.log(`Listening on HTTPS port *:${port}`))
 }
 
-function jsonError(res) {
-  return err => {
-    if (err) {
+function jsonError(res: Response) {
+  return (err: Error | undefined) => {
+    if (err && 'status' in err && err.status && typeof err.status === 'number') {
       res.status(err.status).json({})
     }
   }
 }
 
-function bindRoutes(app, rootFolder) {
+function bindRoutes(app: Express, rootFolder: string): void {
   const sendFileOptions = {root: path.resolve(rootFolder)}
   app.use((req, res) => {
     const filePath = req.url.split('%2f').join('/')
@@ -36,15 +37,21 @@ function bindRoutes(app, rootFolder) {
   })
 }
 
-function startServer(options) {
+interface ServerOptions {
+  port: number
+  root: string
+  https?: {
+    port: number
+    sslKey: string
+    sslCert: string
+  }
+}
+
+export function startServer(options: ServerOptions): void {
   const app = express()
   bindRoutes(app, options.root)
   startHttpServer(app, options.port)
   if (options.https) {
     startHttpsServer(app, options.https.port, options.https.sslKey, options.https.sslCert)
   }
-}
-
-module.exports = {
-  startServer
 }
--- release-test/server/start-server.sh
+++ release-test/server/start-server.sh
@@ -4,7 +4,7 @@ set -e
 
 SERVE_FOLDER=$1
 
-npm i
-node src/index.js --root $SERVE_FOLDER --sslCert ssl/localhost.crt --sslKey ssl/localhost.key &
+npm ci
+node src/index.ts --root $SERVE_FOLDER --sslCert ssl/localhost.crt --sslKey ssl/localhost.key &
 echo $! > server.pid
 sleep 2
