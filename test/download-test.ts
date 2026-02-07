cfdd9f281d6bf04e6eb324be32a6be28c13b0d6b
Heikki Pora <heikki.pora@gmail.com>
2026-02-07 13:42:22 +0200

    Update approba used in download tests to latest

--- test/download-test.ts
+++ test/download-test.ts
@@ -30,39 +30,39 @@ describe('download', () => {
     const packages = [
       {id: 'abbrev@1.1.0', name: 'abbrev', version: '1.1.0'},
       {id: 'abbrev@1.1.1', name: 'abbrev', version: '1.1.1'},
-      {id: 'aproba@1.2.0', name: 'aproba', version: '1.2.0'}
+      {id: 'aproba@2.1.0', name: 'aproba', version: '2.1.0'}
     ]
     await downloadAll(packages, options)
   })
 
   it('Should download a package with correct metadata', async () => {
-    const packages = [{id: 'aproba@1.2.0', name: 'aproba', version: '1.2.0'}]
+    const packages = [{id: 'aproba@2.1.0', name: 'aproba', version: '2.1.0'}]
     await downloadAll(packages, options)
     const aprobaMetadata = await readMetadataFile('aproba')
-    expect(aprobaMetadata.versions['1.2.0'].dist.tarball).equal('https://localhost:8443/aproba/aproba-1.2.0.tgz')
+    expect(aprobaMetadata.versions['2.1.0'].dist.tarball).equal('https://localhost:8443/aproba/aproba-2.1.0.tgz')
   })
 
   it('Should download a package with correct metadata when localUrl contains a path', async () => {
-    const packages = [{id: 'aproba@1.2.0', name: 'aproba', version: '1.2.0'}]
+    const packages = [{id: 'aproba@2.1.0', name: 'aproba', version: '2.1.0'}]
     await downloadAll(packages, {
       ...options,
       localUrl: new URL('https://localhost:8443/registry')
     })
     const aprobaMetadata = await readMetadataFile('aproba')
-    expect(aprobaMetadata.versions['1.2.0'].dist.tarball).equal(
-      'https://localhost:8443/registry/aproba/aproba-1.2.0.tgz'
+    expect(aprobaMetadata.versions['2.1.0'].dist.tarball).equal(
+      'https://localhost:8443/registry/aproba/aproba-2.1.0.tgz'
     )
   })
 
   it('Should download a package with correct metadata when localUrl contains a path with ending slash', async () => {
-    const packages = [{id: 'aproba@1.2.0', name: 'aproba', version: '1.2.0'}]
+    const packages = [{id: 'aproba@2.1.0', name: 'aproba', version: '2.1.0'}]
     await downloadAll(packages, {
       ...options,
       localUrl: new URL('https://localhost:8443/registry/')
     })
     const aprobaMetadata = await readMetadataFile('aproba')
-    expect(aprobaMetadata.versions['1.2.0'].dist.tarball).equal(
-      'https://localhost:8443/registry/aproba/aproba-1.2.0.tgz'
+    expect(aprobaMetadata.versions['2.1.0'].dist.tarball).equal(
+      'https://localhost:8443/registry/aproba/aproba-2.1.0.tgz'
     )
   })
 
